c9c900cfbdb76ba0cf2dac2d9a6ece7f30206ebe
IzaanArain <izaansaquib@gmail.com>
2023-11-08 23:13:22 +0500

    updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-
-=======
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUp from "../pages/SignUp";
-import Login from "../pages/Login";
->>>>>>> 2a97053c5121e542b6564a98173b42f389d9113d
 
 const Layout = () => {
   return (

aa1c6393e54209367008622786cfe276fb7af876
IzaanArain <izaansaquib@gmail.com>
2023-11-15 04:35:26 +0500

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import Logout from './Logout'
 
 const NavBar = () => {
   return (
     <>
-    <Logout/>
+     <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#">BLOG</Navbar.Brand>
+        <Logout/>
+      </Container>
+    </Navbar>
     </>
   )
 }
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import Container from "react-bootstrap/Container";
+import { useState } from "react";
 
 const UserList = () => {
   return (
-   <>
-   <h1>users</h1>
-   </>
-  )
-}
+    <>
+      <Container>
+        <h1>users</h1>
+      </Container>
+    </>
+  );
+};
 
-export default UserList
+export default UserList;

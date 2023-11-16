54465702dc3ae92f20bf322e10879c8cffacf1c5
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-11-16 19:23:43 +0500

    updated

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,17 +7,18 @@ import { AiOutlineMail as MailLogo } from "react-icons/ai";
 import { RiLockPasswordFill as PasswordLogo } from "react-icons/ri";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { signUpApi,signUpUser} from "../features/Auth/Auth";
+import { signUpApi, signUpUser } from "../features/Auth/Auth";
 import { ToastContainer } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const user=useSelector(signUpUser); 
+  const user = useSelector(signUpUser);
   // console.log(user)
 
   const onSubmit = (e) => {
@@ -27,59 +28,84 @@ const SignUp = () => {
         email,
         password,
       })
-    );  
-    navigate("/otp_verify",{state:{email}});
+    );
+    navigate("/otp_verify", { state: { email } });
   };
 
   return (
     <>
       <Container>
         <Row>
-          <Col>
-            <Form onSubmit={onSubmit}>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="email">Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  id="email"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Form.Group>
+          <Col lg={6} xxl={12}>
+            <Card>
+              <Row>
+                <Col>
+                  <Card.Img
+                    src=""
+                    alt=""
+                    onError={(err) => {
+                      err.target.src =
+                        "https://www.state.gov/wp-content/uploads/2019/04/Pakistan-2115x1406.jpg";
+                    }}
+                    width="100%"
+                    height="100%"
+                  />
+                </Col>
 
-              <Form.Group className="mb-3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  id="password"
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
+                <Col>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Form onSubmit={onSubmit}>
+                      <Form.Group className="mb-3">
+                        <Form.Label htmlFor="email">Email address</Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Enter email"
+                          id="email"
+                          name="email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Confirm password"
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Password"
+                          id="password"
+                          name="password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </Form.Group>
 
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Confirm password"
+                          id="confirmPassword"
+                          name="confirmPassword"
+                          value={confirmPassword}
+                          onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                      </Form.Group>
+
+                      <div className="d-grid">
+                        <Button variant="primary" type="submit" size="lg">
+                          Submit
+                        </Button>
+                      </div>
+                      
+                    </Form>
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
           </Col>
         </Row>
-        <ToastContainer/>
+        <ToastContainer />
       </Container>
     </>
   );

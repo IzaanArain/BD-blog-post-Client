806fdd15e17f559c126842047692a4461f8e6817
IzaanArain <izaansaquib@gmail.com>
2023-11-17 08:08:28 +0500

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { AiOutlineMail as MailLogo } from "react-icons/ai";
+import { AiOutlineMail as EmailLogo } from "react-icons/ai";
 import { RiLockPasswordFill as PasswordLogo } from "react-icons/ri";
 import { useState } from "react";
 import { loginApi, loggedInUser } from "../features/Auth/Auth";
@@ -12,6 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordVisibilty, setPasswordVisibilty] = useState(false);
   const dispatch = useDispatch();
   const user = useSelector(loggedInUser);
   // console.log("logged In User", user);
@@ -30,38 +31,51 @@ const Login = () => {
     <>
       <Container>
         <Row>
-          <Col>
+          <Col className="d-flex justify-content-center">
             <Form onSubmit={onSubmit}>
-              <Form.Group className="mb-3" controlId="loginEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
-                />
+              <Form.Group as={Row} className="mb-3 mt-3" controlId="loginEmail">
+                <Form.Label column sm="2">
+                  Email
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+                  />
+                </Col>
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="loginPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-                  title="Password should include at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character."
-                />
+              <Form.Group as={Row} className="mb-3" controlId="loginPassword">
+                <Form.Label column sm="2">
+                  Password
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    aria-describedby="passwordHelpBlock"
+                    pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+                    title="Password should include at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character."
+                  />
+                  <Form.Text id="passwordHelpBlock" muted>
+                    Password should include at least 8 characters, one uppercase
+                    letter, one lowercase letter, one digit, and one special
+                    character.
+                  </Form.Text>
+                </Col>
               </Form.Group>
-
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
+                <Button variant="primary" type="submit" size="sm">
+                  Submit
+                </Button>
             </Form>
           </Col>
         </Row>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -16,108 +16,95 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [passwordVisibilty,setPasswordVisibilty]=useState(false)
+  const [passwordVisibilty, setPasswordVisibilty] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector(signUpUser);
 
   const onSubmit = (e) => {
     e.preventDefault();
-   if(confirmPassword===password){
-    dispatch(
-      signUpApi({
-        email,
-        password,
-      })
-    )
-      .unwrap()
-      .then(() => {
-        navigate("/otp_verify", { state: { email } });
+    if (confirmPassword === password) {
+      dispatch(
+        signUpApi({
+          email,
+          password,
+        })
+      )
+        .unwrap()
+        .then(() => {
+          navigate("/otp_verify", { state: { email } });
+        });
+    } else {
+      toast.error(`password and confirm password do not match`, {
+        position: toast.POSITION.TOP_RIGHT,
       });
-   }else{
-    toast.error(`password and confirm password do not match`, {
-      position: toast.POSITION.TOP_RIGHT,
-    });
-   }
+    }
   };
 
   return (
     <>
       <Container>
         <Row>
-          <Col lg={6} xxl={12}>
-            <Card>
-              <Row>
-                <Col>
-                  <Card.Img
-                    src=""
-                    alt=""
-                    onError={(err) => {
-                      err.target.src =
-                        "https://www.state.gov/wp-content/uploads/2019/04/Pakistan-2115x1406.jpg";
-                    }}
-                    width="100%"
-                    height="100%"
+          <Col>
+            <Form onSubmit={onSubmit}>
+              <Form.Group as={Row} className="mb-3 mt-3">
+                <Form.Label column sm="2" htmlFor="email">
+                  Email address
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
                   />
                 </Col>
+              </Form.Group>
 
-                <Col>
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Form onSubmit={onSubmit}>
-                      <Form.Group className="mb-3">
-                        <Form.Label htmlFor="email">Email address</Form.Label>
-                        <Form.Control
-                          type="email"
-                          placeholder="Enter email"
-                          id="email"
-                          name="email"
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                          required
-                        />
-                      </Form.Group>
-
-                      <Form.Group className="mb-3">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          placeholder="Password"
-                          id="password"
-                          name="password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                          required
-                          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-                          title="Password should include at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character."
-                        />
-                      </Form.Group>
-
-                      <Form.Group className="mb-3">
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          placeholder="Confirm password"
-                          id="confirmPassword"
-                          name="confirmPassword"
-                          value={confirmPassword}
-                          onChange={(e) => setConfirmPassword(e.target.value)}
-                          required
-                          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-                          title="Password should include at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character."
-                        />
-                      </Form.Group>
-
-                      <div className="d-grid">
-                        <Button variant="primary" type="submit" size="lg">
-                          Submit
-                        </Button>
-                      </div>
-                    </Form>
-                  </Card.Body>
+              <Form.Group as={Row} className="mb-3">
+                <Form.Label column sm="2" htmlFor="password">
+                  Password
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+                    title="Password should include at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character."
+                  />
                 </Col>
-              </Row>
-            </Card>
+              </Form.Group>
+
+              <Form.Group as={Row} className="mb-3">
+                <Form.Label column sm="2" htmlFor="confirmPassword">
+                  Confirm Password
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="password"
+                    placeholder="Confirm password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                    pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+                    title="Password should include at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character."
+                  />
+                </Col>
+              </Form.Group>
+                <Button variant="primary" type="submit" size="sm">
+                  Submit
+                </Button>
+            </Form>
           </Col>
         </Row>
       </Container>

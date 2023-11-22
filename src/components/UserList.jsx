5d9b1f5744badbbe5926513749f81f2bf56993df
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-11-22 17:03:07 +0500

    updated

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -5,10 +5,11 @@ import Col from "react-bootstrap/Col";
 import UserCard from "./UserCard";
 import { getAllUsers, getAllUsersApi } from "../features/Chat/ChatSlice";
 import { useDispatch, useSelector } from "react-redux";
-
+import { loggedInUser } from "../features/Auth/Auth";
 const UserList = () => {
   const dispatch = useDispatch();
-
+  const user=useSelector(loggedInUser);
+  const user_id=user?._id
   useEffect(() => {
     dispatch(getAllUsersApi());
   }, [dispatch]);
@@ -18,13 +19,16 @@ const UserList = () => {
     <>
       <div className="user-list">
       {userList.map((user, i) => {
-        return (
-          <Fragment key={i}>
-              <div className="my-3">
-              <UserCard user={user}/>
-              </div>
-          </Fragment>
-        );
+        const id=user?._id
+        if(user_id!==id){
+          return (
+            <Fragment key={i}>
+                <div className="my-3">
+                <UserCard user={user}/>
+                </div>
+            </Fragment>
+          );
+        }
       })}
       </div>
     </>

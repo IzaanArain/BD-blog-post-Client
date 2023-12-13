37fdcf2c9ad03e72d92c80600293e96841fc0111
IzaanArain <izaansaquib@gmail.com>
2023-12-14 00:30:54 +0500

    updated

--- src/features/Chat/ChatSlice.jsx
+++ src/features/Chat/ChatSlice.jsx
@@ -3,10 +3,10 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const user = JSON.parse(localStorage.getItem("user")) //? JSON.parse(localStorage.getItem("user")) : "";
-const token = user?.user_auth;
+// const user = JSON.parse(localStorage.getItem("user")) //? JSON.parse(localStorage.getItem("user")) : "";
+// const token = user?.user_auth;
 // axios.defaults.headers.common['Authorization']=`Bearer ${token}`;
-console.log("token",token)
+// console.log("token", token);
 
 const initialState = {
   isLoading: false,
@@ -21,13 +21,15 @@ export const getAllUsersApi = createAsyncThunk(
   "auth/all_users",
   async (payload, thunkAPI) => {
     try {
-      const res = await axios.get(`${url}/all_users`,{
-        headers:{
-          "Authorization":`Bearer ${token}`
-        }
+      const user = JSON.parse(localStorage.getItem("user"));
+      const token = user?.user_auth;
+      const res = await axios.get(`${url}/all_users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
       const data = await res.data;
-      console.log("getAllUsersApi",data)
+      console.log("getAllUsersApi", data);
       return { data };
     } catch (err) {
       return thunkAPI.rejectWithValue(err.response.data);
@@ -40,29 +42,29 @@ const ChatSlice = createSlice({
   initialState,
   reducers: {},
   // extraReducers
-  extraReducers:(builder)=>{
+  extraReducers: (builder) => {
     builder
-    .addCase(getAllUsersApi.pending,(state,action)=>{
-      state.isLoading = true;
-      state.isError = false;
-    })
-    .addCase(getAllUsersApi.fulfilled,(state,action)=>{
-      state.isLoading = false;
-      state.isError = false;
-      state.users=action?.payload?.data?.users
-      // toast.success(`${action?.payload?.data?.message}`, {
-      //   position: toast.POSITION.TOP_RIGHT,
-      // });
-    })
-    .addCase(getAllUsersApi.rejected,(state,action)=>{
-      state.isLoading = false;
-      state.isError = true;
-      console.log(action?.payload)
-      toast.error(`${action?.payload?.message}`, {
-        position: toast.POSITION.TOP_RIGHT,
+      .addCase(getAllUsersApi.pending, (state, action) => {
+        state.isLoading = true;
+        state.isError = false;
+      })
+      .addCase(getAllUsersApi.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.users = action?.payload?.data?.users;
+        // toast.success(`${action?.payload?.data?.message}`, {
+        //   position: toast.POSITION.TOP_RIGHT,
+        // });
+      })
+      .addCase(getAllUsersApi.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        console.log(action?.payload);
+        toast.error(`${action?.payload?.message}`, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
       });
-    })
-  }
+  },
 });
 
 export const getAllUsers = (state) => state?.chat?.users;
